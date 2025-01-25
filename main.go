5b2adb52de0452ae56a3690142fd22046b90c2db
Devin Collins <me@imdevinc.com>
2025-01-24 19:37:34 -0800

    add logging

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ const defaultPort = 8080
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api", handler)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("[ERROR] invalid request")
+		log.Println(r.URL.String())
+		w.WriteHeader(http.StatusNotFound)
+	})
 	rawPort := os.Getenv("DND_PORT")
 	port, err := strconv.ParseInt(rawPort, 10, 64)
 	if err != nil {
@@ -25,6 +30,8 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	character := r.URL.Query().Get("character")
 	if len(character) == 0 {
+		log.Println("[ERROR] invalid request")
+		log.Println(r.URL.String())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
