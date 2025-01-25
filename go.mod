d3f215285b2b1d13d0b6e4593855f6f3f3a16997
Devin Collins <me@imdevinc.com>
2025-01-24 19:09:35 -0800

    fix(base): initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/imdevinc/dnd-proxy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const defaultPort = 8080
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", handler)
+	rawPort := os.Getenv("DND_PORT")
+	port, err := strconv.ParseInt(rawPort, 10, 64)
+	if err != nil {
+		port = defaultPort
+	}
+	log.Println("[INFO] listening on port ", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	character := r.URL.Query().Get("character")
+	if len(character) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	destinationUrl := "https://character-service.dndbeyond.com/character/v5/character/" + character
+	w.Header().Add("Location", destinationUrl)
+	w.WriteHeader(http.StatusPermanentRedirect)
+}
